2026a3861544a2ab5e819e5a7459559d3a49310a
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2023-10-27 10:57:42 +0800

    Modified HTML files and added base HTML file

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
 
     @app.route("/")
     def index():
-        return render_template('login.html')
+        return render_template('base.html')
 
     from app.routes.admin import admin_bp
     from app.routes.doctor import doctor_bp
