6fa00a142c5abaed9f1cca27a5dbfdbbd389cf00
kycx0236 <nollan.kurtyuri@s.msumain.edu.ph>
2023-11-03 22:57:03 +0800

    Modified the forms

--- app/forms/receptionist_f.py
+++ app/forms/receptionist_f.py
@@ -4,12 +4,17 @@ from wtforms import StringField, validators, SubmitField
 
 class AppointmentForm(FlaskForm):
     reference_number = StringField('reference_number', [validators.DataRequired(), validators.Length(min=5, max=20)])
-    date_appointment = StringField('date_appointment', [validators.Length(min=8, max=50)])
-    time_appointment = StringField('time_appointment', [validators.Length(min=6, max=50)])
+    date_appointment = StringField('date_appointment', [validators.Length(min=4, max=50)])
+    time_appointment = StringField('time_appointment', [validators.Length(min=4, max=50)])
     status_ = StringField('status_', [validators.Length(min=4, max=50)])
-    book_date = StringField('book_date', [validators.Length(min=8, max=50)])
-    patient_name = StringField('patient_name', [validators.Length(min=8, max=50)])
-    birthdate = StringField('birthdate', [validators.Length(min=8, max=50)])
+    book_date = StringField('book_date', [validators.Length(min=4, max=50)])
+    first_name = StringField('first_name', [validators.Length(min=2, max=50)])
+    middle_name = StringField('middle_name', [validators.Length(min=2, max=50)])
+    last_name = StringField('last_name', [validators.Length(min=2, max=50)])
+    sex = StringField('sex', [validators.Length(max=1)])
+    birth_date = StringField('birth_date', [validators.Length(min=8, max=50)])
     contact_number = StringField('contact_number', [validators.Length(min=11, max=50)])
     email = StringField('email', [validators.Length(min=10, max=50)])
+    address = StringField('address', [validators.Length(min=10, max=50)])
     submit = SubmitField("Submit")
+
