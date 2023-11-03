4c4e0f572010046324ad36277a5a4644584b26a1
kycx0236 <nollan.kurtyuri@s.msumain.edu.ph>
2023-11-03 22:57:45 +0800

    Modified the functions inside the model

--- app/models/receptionist_m.py
+++ app/models/receptionist_m.py
@@ -19,13 +19,13 @@ class Appointment:
     def add(self):
         try:
             cursor = mysql.connection.cursor()
-            sql = "INSERT INTO students (id_number, first_name, last_name, course_code, year_, gender) VALUES (%s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (self.id_number, self.first_name, self.last_name, self.course_code, self.year_, self.gender))
+            sql = "INSERT INTO appointment (reference_number, date_appointment, time_appointment, status_, book_date, first_name, middle_name, last_name, sex, birth_date, contact_number, email, address) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (self.reference_number, self.date_appointment, self.time_appointment, self.status_, self.book_date, self.first_name, self.middle_name, self.last_name, self.sex, self.birth_date, self.contact_number, self.email, self.address))
             mysql.connection.commit()
             return True
         except Exception as e:
             # You might want to log this error for debugging purposes
-            print(f"Error adding student: {e}")
+            print(f"Error adding appointment: {e}")
             return False
 
     @classmethod
@@ -38,53 +38,53 @@ class Appointment:
             return result
         except Exception as e:
             # You might want to log this error for debugging purposes
-            print(f"Error fetching all students: {e}")
+            print(f"Error fetching all appoinments: {e}")
             return []
 
     @classmethod
-    def delete(cls, id_number):
+    def delete(cls, reference_number):
         try:
             cursor = mysql.connection.cursor()
-            sql = "DELETE FROM students WHERE id_number = %s"
-            cursor.execute(sql, (id_number,))
+            sql = "DELETE FROM appointment WHERE reference_number = %s"
+            cursor.execute(sql, (reference_number,))
             mysql.connection.commit()
             return True
         except Exception as e:
             # You might want to log this error for debugging purposes
-            print(f"Error deleting student: {e}")
+            print(f"Error deleting appointment: {e}")
             return False
 
     @classmethod
-    def update(cls, id_number, new_first_name, new_last_name, new_course_code, new_year_, new_gender):
+    def update(cls, reference_number, new_date_appointment, new_time_appointment, new_status_, new_book_date, new_first_name, new_middle_name, new_last_name, new_sex, new_birth_date, new_contact_number, new_email, new_address):
         try:
             cursor = mysql.connection.cursor()
-            sql = "UPDATE students SET first_name = %s, last_name = %s, course_code = %s, year_ = %s, gender = %s WHERE id_number = %s"
-            cursor.execute(sql, (new_first_name, new_last_name, new_course_code, new_year_, new_gender, id_number))
+            sql = "UPDATE appointment SET date_appointment = %s, time_appointment = %s, status_ = %s, book_date = %s, first_name = %s, middle_name = %s, last_name = %s, sex = %s, birth_date = %s, contact_number = %s, email = %s, address = %s WHERE reference_number = %s"
+            cursor.execute(sql, (new_date_appointment, new_time_appointment, new_status_, new_book_date, new_first_name, new_middle_name, new_last_name, new_sex, new_birth_date, new_contact_number, new_email, new_address, reference_number))
             mysql.connection.commit()
             return True
         except Exception as e:
             # You might want to log this error for debugging purposes
-            print(f"Error updating student: {e}")
+            print(f"Error updating appointment: {e}")
             return False
         
     @classmethod
-    def unique_code(cls, id_number):
+    def unique_code(cls, reference_number):
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT id_number FROM students WHERE id_number = %s", (id_number,))
+        cursor.execute("SELECT reference_number FROM appointment WHERE reference_number = %s", (reference_number,))
         code = cursor.fetchone()  # Use fetchone() to get a single result
         cursor.close()
         return code
     
     @classmethod
-    def get_student_by_id(cls, id_number):
+    def get_appointment_by_reference(cls, reference_number):
         cursor = mysql.connection.cursor(dictionary=True)  # Set dictionary=True to return results as dictionaries
-        cursor.execute("SELECT * FROM students WHERE id_number = %s", (id_number,))
-        student_data = cursor.fetchone()
+        cursor.execute("SELECT * FROM appointment WHERE reference_number = %s", (reference_number,))
+        appointment_data = cursor.fetchone()
         cursor.close()
-        return student_data
+        return appointment_data
 
     @classmethod
-    def search_student(cls, query):
+    def search_appointment(cls, query):
         try:
             with mysql.connection.cursor() as cursor:
                 sql = """
@@ -107,7 +107,7 @@ class Appointment:
             return []
 
     @classmethod
-    def filter_student(cls, filter_by, query):
+    def filter_appointment(cls, filter_by, query):
         try:
             with mysql.connection.cursor() as cursor:
                 # Construct the SQL query based on the selected column
