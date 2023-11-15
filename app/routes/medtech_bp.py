67571085633c050f5a1c8b424a62bf92aa7ff7e2
Lance Lompon <xerxeslancelaurenz.lompon@g.msuiit.edu.ph>
2023-11-15 22:02:38 +0800

    incorporate login required and role required for every route

--- app/routes/medtech_bp.py
+++ app/routes/medtech_bp.py
@@ -3,21 +3,32 @@ from app.forms.medtech_f import *
 import app.models as models
 from app.models.medtech_m import *
 from flask import Blueprint
+from flask_login import login_required, logout_user
+from app.routes.utils import role_required
 
 medtech_bp = Blueprint('medtech', __name__)
 
 @medtech_bp.route('/')
+@login_required
+@role_required('medtech')
 def dashboard():
     return render_template("medtech/dashboard.html")
 
-@medtech_bp.route('/patient/')
+@medtech_bp.route('/patient')
+@login_required
+@role_required('medtech')
 def patient():
     return render_template("medtech/patient.html")
 
-@medtech_bp.route('/profile/')
+@medtech_bp.route('/profile')
+@login_required
+@role_required('medtech')
 def profile():
     return render_template("medtech/profile.html")
 
-@medtech_bp.route('/login/')
+@medtech_bp.route('/logout')
+@login_required
+@role_required('medtech')
 def logout():
-    return render_template("login.html")
+    logout_user()
+    return redirect(url_for('login'))
--- app/routes/receptionist_bp.py
+++ app/routes/receptionist_bp.py
@@ -3,25 +3,38 @@ from app.forms.receptionist_f import *
 import app.models as models
 from app.models.receptionist_m import *
 from flask import Blueprint
+from flask_login import login_required, logout_user
+from app.routes.utils import role_required
 
 receptionist_bp = Blueprint('receptionist', __name__)
 
 @receptionist_bp.route('/')
+@login_required
+@role_required('receptionist')
 def dashboard():
     return render_template("receptionist/dashboard.html")
 
-@receptionist_bp.route('/calendar/')
+@receptionist_bp.route('/calendar')
+@login_required
+@role_required('receptionist')
 def calendar():
     return render_template("receptionist/calendar.html")
 
-@receptionist_bp.route('/appointment/')
+@receptionist_bp.route('/appointment')
+@login_required
+@role_required('receptionist')
 def appointment():
     return render_template("receptionist/appointment.html")
 
-@receptionist_bp.route('/profile/')
+@receptionist_bp.route('/profile')
+@login_required
+@role_required('receptionist')
 def profile():
     return render_template("receptionist/profile.html")
 
-@receptionist_bp.route('/login/')
+@receptionist_bp.route('/logout')
+@login_required
+@role_required('receptionist')
 def logout():
-    return render_template("login.html")
+    logout_user()
+    return redirect(url_for('login'))
