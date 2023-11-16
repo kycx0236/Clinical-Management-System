cb04277c3632699d7df01d2b0f6bb770d88d2540
Eda Grace Paragoso <edagrace.paragoso@g.msuiit.edu.ph>
2023-11-16 21:01:51 +0800

    Added more fields in doctor and medtech's forms.

--- app/forms/medtech_f.py
+++ app/forms/medtech_f.py
@@ -1,15 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, SelectField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class PatientForm(FlaskForm):
-    orderID = IntegerField('Order ID', validators=[DataRequired()])
-    patientID = IntegerField('Patient ID')
-    patientName = StringField('Patient Name', validators=[DataRequired(), Length(max=255)])
-    gender = StringField('Gender', validators=[DataRequired(), Length(max=10)])
-    age = StringField('Age', validators=[DataRequired(), Length(max=10)])
-    physician = StringField('Physician', validators=[DataRequired(), Length(max=255)])
-    orderDate = StringField('Order Date', validators=[DataRequired(), Length(max=20)])
-    otherTest = StringField('Other Test', validators=[Length(max=255)])
+
+    # LABORATORY TEST RESULTS
+    processName = StringField('Process Name', validators=[DataRequired(), Length(max=255)])
+    testResult = StringField('Test Result', validators=[DataRequired(), Length(max=255)])
+    refValue = StringField('Reference Value', validators=[DataRequired(), Length(max=255)])
+    diagnosisReport = StringField('Diagnosis Report', validators=[DataRequired(), Length(max=255)])
     
     submit = SubmitField('Submit')
