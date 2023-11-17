64b26f238fd412a08bd9531de9e255def2f24085
kycx0236 <nollan.kurtyuri@s.msumain.edu.ph>
2023-11-17 13:09:38 +0800

    Delete icon functions added

--- app/static/receptionist_script.js
+++ app/static/receptionist_script.js
@@ -1,10 +1,14 @@
+// Selectors
 const searchInput = document.querySelector('.doccare-input-group input');
 const tableRows = document.querySelectorAll('tbody tr');
-const table_headings = document.querySelectorAll('thead th');
+const tableHeadings = document.querySelectorAll('thead th');
+const deleteModal = document.getElementById('deleteModal');
 
-
+// Event listeners
 searchInput.addEventListener('input', searchTable);
+tableHeadings.forEach((head, i) => head.addEventListener('click', () => handleSortClick(i)));
 
+// Search functionality
 function searchTable() {
   const searchValue = searchInput.value.toLowerCase();
 
@@ -16,9 +20,7 @@ function searchTable() {
     row.style.setProperty('--delay', i / 25 + 's');
 
     // Reset background color for even rows
-    if (i % 2 === 0) {
-      row.style.backgroundColor = '#ecf6ff';
-    }
+    row.style.backgroundColor = i % 2 === 0 ? '#ecf6ff' : '';
   });
 
   const visibleRows = document.querySelectorAll('tbody tr:not(.hide)');
@@ -27,31 +29,110 @@ function searchTable() {
   });
 }
 
-table_headings.forEach((head, i) => {
-    let sort_asc = true;
-    head.onclick = () => {
-        table_headings.forEach(head => head.classList.remove('active'));
-        head.classList.add('active');
+// Sorting functionality
+function handleSortClick(column) {
+  tableHeadings.forEach(head => head.classList.remove('active', 'asc'));
+  tableRows.forEach(row => row.querySelectorAll('td')[column].classList.remove('active'));
 
-        document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
-        tableRows.forEach(row => {
-            row.querySelectorAll('td')[i].classList.add('active');
-        })
+  tableHeadings[column].classList.add('active', 'asc');
+  tableRows.forEach(row => row.querySelectorAll('td')[column].classList.add('active'));
 
-        head.classList.toggle('asc', sort_asc);
-        sort_asc = head.classList.contains('asc') ? false : true;
-
-        sortTable(i, sort_asc);
-    }
-})
-
-
-function sortTable(column, sort_asc) {
-    [...tableRows].sort((a, b) => {
-        let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
-            second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
-
-        return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
-    })
-        .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
+  sortTable(column, !tableHeadings[column].classList.contains('asc'));
 }
+
+function sortTable(column, sortAsc) {
+  [...tableRows].sort((a, b) => {
+    const firstRow = a.querySelectorAll('td')[column].textContent.toLowerCase();
+    const secondRow = b.querySelectorAll('td')[column].textContent.toLowerCase();
+
+    return sortAsc ? (firstRow < secondRow ? 1 : -1) : (firstRow < secondRow ? -1 : 1);
+  }).forEach(sortedRow => document.querySelector('tbody').appendChild(sortedRow));
+}
+
+// Select the delete icon by its class or another appropriate selector
+const deleteIcons = document.querySelectorAll('.delete-appoinment-button');
+
+// Add a click event listener to each delete icon
+deleteIcons.forEach(deleteIcon => {
+    deleteIcon.addEventListener('click', function(event) {
+        // Prevent the default action of the delete icon
+        event.preventDefault();
+
+        // Get the appointment ID from the data-id attribute
+        const appointmentId = this.getAttribute('data-id');
+
+        // Open the delete modal
+        openDeleteModal(appointmentId);
+    });
+});
+
+// Delete functionality
+function openDeleteModal(appointmentId) {
+  deleteModal.setAttribute('data-appointment-id', appointmentId);
+  deleteModal.style.display = 'flex';
+  toggleOverlayBackground(true);
+}
+
+function closeDeleteModal() {
+  deleteModal.style.display = 'none';
+  toggleOverlayBackground(false); // Close the overlay background when closing the modal
+}
+
+// Function to toggle the overlay background
+function toggleOverlayBackground(show) {
+  const overlayBackgroundContainer = document.querySelector('.overlay-background-container');
+  overlayBackgroundContainer.style.display = show ? 'flex' : 'none';
+}
+
+function deleteAppointment() {
+  // Assuming deleteModal is your modal element
+  const appointmentId = deleteModal.getAttribute('data-appointment-id');
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+  // Make an AJAX request to delete the appointment
+  $.ajax({
+      type: 'POST',
+      url: '/receptionist/delete-appointment/',
+      data: { reference_number: appointmentId },
+      headers: {
+        "X-CSRFToken": csrfToken,// Include the CSRF token in the headers
+      },
+      success: function(response) {
+          // Check the response from the server
+          if (response.success) {
+              const appointmentRow = document.getElementById('row-' + appointmentId);
+              if (appointmentRow) {
+                  appointmentRow.remove();
+              }
+
+              // Display a success message or handle as needed
+              alert('Appointment deleted successfully!');
+              window.location.reload(); // Refresh the page after a successful delete
+          } else {
+              // Display an error message or handle as needed
+              alert('Failed to delete appointment.');
+          }
+          
+          // Close the delete modal
+          closeDeleteModal();
+      },
+      error: function() {
+          // Display an error message or handle as needed
+          alert('Error occurred while processing the request.');
+          
+          // Close the delete modal
+          closeDeleteModal();
+      }
+  });
+}
+
+// Close the delete modal if the user clicks outside of it
+window.addEventListener('click', event => {
+  if (event.target === deleteModal) {
+    closeDeleteModal();
+  }
+});
+
+// Add event listener to cancel button
+const cancelModalButton = document.querySelector('.cancel-modal-button');
+cancelModalButton.addEventListener('click', closeDeleteModal);
+
