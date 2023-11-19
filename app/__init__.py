375409d1302454e5943c60a4019c0e9fcb7d8c9f
KURT YURI NOLLAN <kurtyuri.nollan@g.msuiit.edu.ph>
2023-11-20 01:03:43 +0800

    Log in module and function added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysql_connector import MySQL
-from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, login_user
 
 mysql = MySQL()
-bootstrap = Bootstrap()
 
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -18,23 +18,48 @@ def create_app():
     app.config["MYSQL_DATABASE"] = DB_NAME
 
     mysql.init_app(app)
-    bootstrap.init_app(app)
     
     # Initialize CSRF protection before registering blueprints
     CSRFProtect(app)
-    
-    @app.route("/")
-    def index():
+    login_manager = LoginManager(app)
+ 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
+
+    @app.route("/", methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            user = User.authenticate(username, password)
+
+            if user:
+                login_user(user)
+                if user.role == 'admin':
+                    return redirect(url_for('admin.dashboard'))
+                elif user.role == 'doctor':
+                    return redirect(url_for('doctor.dashboard'))
+                elif user.role == 'medtech':
+                    return redirect(url_for('medtech.dashboard'))
+                elif user.role == 'receptionist':
+                    return redirect(url_for('receptionist.dashboard'))
+            else:
+                flash("The username or password you've entered is incorrect", 'error')
+
         return render_template("login.html")
+
 
     from app.routes.admin_bp import admin_bp
     from app.routes.doctor_bp import doctor_bp
     from app.routes.medtech_bp import medtech_bp
     from app.routes.receptionist_bp import receptionist_bp
-
-    app.register_blueprint(admin_bp, url_prefix="/admin/")
-    app.register_blueprint(doctor_bp, url_prefix="/doctor/")
-    app.register_blueprint(medtech_bp, url_prefix="/medtech/")
-    app.register_blueprint(receptionist_bp, url_prefix="/receptionist/")
+    from app.models.login_m import User
+    
+    app.register_blueprint(admin_bp, url_prefix='/admin/')
+    app.register_blueprint(doctor_bp, url_prefix='/doctor/')
+    app.register_blueprint(medtech_bp, url_prefix='/medtech/')
+    app.register_blueprint(receptionist_bp, url_prefix='/receptionist/')
 
     return app
