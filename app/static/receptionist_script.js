187198212ae12da3dd9cdc9a895aef61c206138f
KURT YURI NOLLAN <kurtyuri.nollan@g.msuiit.edu.ph>
2023-11-25 10:12:28 +0800

    Status function and colors updated

--- app/static/receptionist_script.js
+++ app/static/receptionist_script.js
@@ -154,8 +154,8 @@ function applyStatusStyles() {
               cell.style.paddingLeft = '15px'; // Adjust the value as needed
               break;
           case 'cancelled':
-              cell.style.backgroundColor = '#d893a3';
-              cell.style.color = '#b30021';
+              cell.style.backgroundColor = '#FFCCCB';
+              cell.style.color = '#F00';
               cell.style.paddingLeft = '15px'; // Adjust the value as needed
               break;
           case 'done':
@@ -166,6 +166,11 @@ function applyStatusStyles() {
           case 'scheduled':
               cell.style.backgroundColor = '#DFF1FF';
               cell.style.color = '#0277BD';
+              cell.style.paddingLeft = '15px'; // Adjust the value as needed
+              break;
+          case 'rescheduled':
+              cell.style.backgroundColor = '#FFC';
+              cell.style.color = '#7A760A';
               cell.style.paddingLeft = '15px'; // Adjust the value as needed
               break;
           // Add more cases as needed
