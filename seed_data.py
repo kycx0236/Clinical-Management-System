c07dd9e61b9a75c5292fff3f65b6930ae5f0aec7
Eda Grace Paragoso <edagrace.paragoso@g.msuiit.edu.ph>
2024-01-16 17:10:27 +0800

    Changed the passwords to default in seeding.

--- seed_data.py
+++ seed_data.py
@@ -12,23 +12,18 @@ def seed_data():
 
         try:
             users_data = [
-                {'username': 'admin1', 'first_name': 'admin', 'middle_name': 'admin', 'last_name': 'admin', 'email': 'user1@example.com', 'gender': 'male', 'user_role': 'admin'},
+                {'username': 'admin1', 'password': 'admin1', 'first_name': 'admin', 'middle_name': 'admin', 'last_name': 'admin', 'email': 'user1@example.com', 'gender': 'male', 'user_role': 'admin'},
 
-                {'username': 'doctor1', 'first_name': 'doctor', 'middle_name': 'doctor', 'last_name': 'doctor', 'email': 'user2@example.com', 'gender': 'female', 'user_role': 'doctor'},
+                {'username': 'doctor1', 'password': 'doctor1', 'first_name': 'doctor', 'middle_name': 'doctor', 'last_name': 'doctor', 'email': 'user2@example.com', 'gender': 'female', 'user_role': 'doctor'},
 
-                {'username': 'medtech1', 'first_name': 'medtech', 'middle_name': 'medtech', 'last_name': 'medtech', 'email': 'user3@example.com', 'gender': 'male', 'user_role': 'medtech'},
+                {'username': 'medtech1', 'password': 'medtech1', 'first_name': 'medtech', 'middle_name': 'medtech', 'last_name': 'medtech', 'email': 'user3@example.com', 'gender': 'male', 'user_role': 'medtech'},
 
-                {'username': 'receptionist1', 'first_name': 'receptionist', 'middle_name': 'receptionist', 'last_name': 'receptionist', 'email': 'user4@example.com', 'gender': 'female', 'user_role': 'receptionist'},
+                {'username': 'receptionist1', 'password': 'receptionist1', 'first_name': 'receptionist', 'middle_name': 'receptionist', 'last_name': 'receptionist', 'email': 'user4@example.com', 'gender': 'female', 'user_role': 'receptionist'},
             ]
 
             for user_data in users_data:
-                user_data['password'] = admin.generate_password()
-                hashed_password = generate_password_hash(user_data['password'])
-
                 query = """INSERT INTO users (username, password, first_name, middle_name, last_name, email, gender, user_role) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
-                cursor.execute(query, (user_data['username'], hashed_password, user_data['first_name'], user_data['middle_name'], user_data['last_name'], user_data['email'], user_data['gender'], user_data['user_role']))
-
-                admin.send_message(user_data['email'], user_data['password'])
+                cursor.execute(query, (user_data['username'], user_data['password'], user_data['first_name'], user_data['middle_name'], user_data['last_name'], user_data['email'], user_data['gender'], user_data['user_role']))
 
             conn.commit()
 
